1f2ff8d73fab16e41f30b31b1e2e89c4fef10388
josephinekoe <koejosephine@gmail.com>
2021-01-10 11:09:11 -0800

    splitting install into ros and packages

--- package_install.bash
+++ package_install.bash
@@ -1,17 +1,3 @@
-echo "****************************** Installing ROS Noetic ******************************"
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
-sudo apt install ros-noetic-desktop-full
-
-sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstool build-essential python3-wstool
-sudo rosdep init
-rosdep update
-
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
 echo "****************************** Installing MoveIt! ******************************"
 
 mkdir -p ~/rethink_ws/src
@@ -19,7 +5,6 @@ cd ~/rethink_ws/src && catkin_init_workspace
 cd ~/rethink_ws && catkin_make
 
 echo "source ~/rethink_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 
 sudo apt install ros-noetic-moveit
 sudo apt install ros-noetic-ros-controllers
@@ -77,4 +62,3 @@ git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 cd ~/turtlebot3_ws && catkin_make
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
-source ~/.bashrc

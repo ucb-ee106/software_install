285cebf872733400cb3d347f9338e8db291bd330
josephinekoe <koejosephine@gmail.com>
2021-01-10 22:59:28 -0800

    Update ros_install.bash

--- ros_install.bash
+++ ros_install.bash
@@ -1,6 +1,6 @@
 echo "****************************** Installing ROS Noetic ******************************"
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 yes | sudo apt install ros-noetic-desktop-full

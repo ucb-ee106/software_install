99b260ac99229c7beafbdcad60124c31c48cd91d
josephinekoe <koejosephine@gmail.com>
2021-01-17 13:58:12 -0800

    Update package_install.bash

--- package_install.bash
+++ package_install.bash
@@ -1,19 +1,21 @@
-echo "****************************** Install MoveIt! and Baxter MoveIt! Configuration ******************************"
+echo "****************************** Install Packages ******************************"
 
-echo "********************Create a workspace for the Rethink packages"
-mkdir -p ~/rethink_ws/src
+echo "********************Create a workspace for the packages"
+mkdir -p ~/106b_packages_ws/src
 
-echo "********************Initialize Rethink workspace"
-cd ~/rethink_ws/src && catkin_init_workspace
+echo "********************Initialize workspace"
+cd ~/106b_packages_ws/src && catkin_init_workspace
 
-echo "********************Build Rethink workspace"
-cd ~/rethink_ws && catkin_make
+echo "********************Build workspace"
+cd ~/106b_packages_ws && catkin_make
 
 echo "********************Source workspace in .bashrc file"
-echo "source ~/rethink_ws/devel/setup.bash" >> ~/.bashrc
+echo "source ~/106b_packages_ws/devel/setup.bash" >> ~/.bashrc
 
 echo "********************Source .bashrc file"
 source ~/.bashrc
+
+echo "****************************** Install MoveIt! and Baxter MoveIt! Configuration ******************************"
 
 echo "********************Install MoveIt!"
 yes | sudo apt install ros-noetic-moveit
@@ -25,10 +27,10 @@ echo "********************Install SLAM package"
 yes | sudo apt install ros-noetic-slam-gmapping
 
 echo "********************Create a directory for Baxter MoveIt! Configuration"
-mkdir -p ~/rethink_ws/src/moveit_robots
+mkdir -p ~/106b_packages_ws/src/moveit_robots
 
 echo "********************Move to Baxter MoveIt! Configuration directory"
-cd ~/rethink_ws/src/moveit_robots
+cd ~/106b_packages_ws/src/moveit_robots
 
 echo "********************Create an empty git repository"
 git init
@@ -50,8 +52,8 @@ git pull origin kinetic-devel
 
 echo "****************************** Install Sawyer Packages ******************************"
 
-echo "********************Move to src directory of Rethink workspace"
-cd ~/rethink_ws/src
+echo "********************Move to src directory of workspace"
+cd ~/106b_packages_ws/src
 
 echo "********************Run wstool init"
 wstool init
@@ -63,12 +65,12 @@ echo "********************Run wstool update"
 wstool update
 
 echo "********************Create a symlink for intera.sh"
-ln -s ~/rethink_ws/src/intera_sdk/intera.sh ~/rethink_ws/intera.sh
+ln -s ~/106b_packages_ws/src/intera_sdk/intera.sh ~/106b_packages_ws/intera.sh
 
 echo "****************************** Installing Baxter Packages ******************************"
 
-echo "********************Move to src directory of Rethink workspace"
-cd ~/rethink_ws/src
+echo "********************Move to src directory of workspace"
+cd ~/106b_packages_ws/src
 
 echo "********************Install Baxter packages"
 yes | wstool merge https://raw.githubusercontent.com/ucb-ee106/baxter_simulator/kinetic-devel/baxter_simulator.rosinstall
@@ -80,15 +82,12 @@ echo "********************Build Rethink workspace"
 cd ~/rethink_ws && catkin_make
 
 echo "********************Create a symlink for baxter.sh"
-ln -s ~/rethink_ws/src/baxter/baxter.sh ~/rethink_ws/baxter.sh
+ln -s ~/106b_packages_ws/src/baxter/baxter.sh ~/106b_packages_ws/baxter.sh
 
 echo "****************************** Installing TurtleBot3 ******************************"
 
-echo "********************Create a workspace for the TurtleBot3 packages"
-mkdir -p ~/turtlebot3_ws/src
-
-echo "********************Move to src directory of TurtleBot3 workspace"
-cd ~/turtlebot3_ws/src
+echo "********************Move to src directory of workspace"
+cd ~/106b_packages_ws/src
 
 echo "********************Clone turtlebot3 repository"
 git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
@@ -99,34 +98,22 @@ git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 echo "********************Clone turtlebot3_simulations repository"
 git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 
-echo "********************Build TurtleBot3 workspace"
-cd ~/turtlebot3_ws && catkin_make
-
-echo "********************Source workspace in .bashrc file"
-echo "source ~/turtlebot3_ws/devel/setup.bash" >> ~/.bashrc
-
 echo "********************Set the TurtleBot3 model to Burger"
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 
-echo "********************Source .bashrc file"
-source ~/.bashrc
-
 echo "****************************** Installing AR Tags Package ******************************"
 
-echo "********************Create a workspace for the ar_tags package"
-mkdir -p ~/ar_tags_ws/src
-
-echo "********************Initialize AR Tags workspace"
-cd ~/ar_tags_ws/src && catkin_init_workspace
+echo "********************Move to src directory of workspace"
+cd ~/106b_packages_ws/src
 
 echo "********************Clone ar_tags repository"
 git clone https://github.com/ucb-ee106/ar_tags
 
-echo "********************Build AR Tags workspace"
-cd ~/ar_tags_ws && catkin_make
+echo "********************Build workspace"
+cd ~/106b_packages_ws && catkin_make
 
-echo "********************Source workspace in .bashrc file"
-echo "source ~/ar_tags_ws/devel/setup.bash" >> ~/.bashrc
+echo "********************Set Gazebo Model Path"
+echo "export GAZEBO_MODEL_PATH=~/106b_packages_ws/src/ar_tags/models:$GAZEBO_MODEL_PATH" >> ~/.bashrc
 
 echo "********************Source .bashrc file"
 source ~/.bashrc
